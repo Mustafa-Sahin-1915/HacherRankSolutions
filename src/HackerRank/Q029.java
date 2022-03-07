f89438eb5a5d9c263cabb1f281995865a8e49621
M Sah <msahin@comu.edu.tr>
2022-03-07 16:49:06 +0300

    ok

--- src/HackerRank/Q029.java
+++ src/HackerRank/Q029.java
@@ -14,7 +14,8 @@ The Player class is provided for you in your editor. It has  fields: a  String a
 Given an array of  Player objects, write a comparator that sorts them in order of decreasing score;
 if  or more players have the same score, sort those players alphabetically by name.
 To do this, you must create a Checker class that implements the Comparator interface,
-then write an int compare(Player a, Player b) method implementing the Comparator.compare(T o1, T o2) method.
+then write an int compare(Player a, Player b) method implementing the
+Comparator.compare(T o1, T o2) method.
 
 Input Format
 
